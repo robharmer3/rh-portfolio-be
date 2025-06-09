41960e02e057b70e36bdc1d4f24f640e78c32a49
robharmer3 <rob.harmer3@gmail.com>
2025-06-09 15:14:51 +0100

    test data added

---
+++ data/test-data/categoryData.js
@@ -0,0 +1,7 @@
+module.exports = [
+  { name: "Northcoder", description: "Northcoder's final project" },
+  {
+    name: "Personal",
+    description: "Person project after completing the Northcoders Bootcamp",
+  },
+];
---
+++ data/test-data/projectsData.js
@@ -0,0 +1,22 @@
+module.export = [
+  {
+    title: "NC News",
+    category: "Northcoder",
+    description: "Final solo project",
+  },
+  {
+    title: "2Gather",
+    category: "Northcoder",
+    description: "Final group project",
+  },
+  {
+    title: "Portfolio",
+    category: "Personal",
+    description: "This portfolio project",
+  },
+  {
+    title: "Top-Trumps",
+    category: "Personal",
+    description: "First attempt at making a game",
+  },
+];
