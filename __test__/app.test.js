a31531cafcb210b11ee91b19d9ba8c2c69c03c53
robharmer3 <rob.harmer3@gmail.com>
2025-06-10 17:08:40 +0100

    app creation & testing started

---
+++ __test__/app.test.js
@@ -0,0 +1,25 @@
+const request = require("supertest");
+
+const app = require("../app");
+const seed = require("../db/seeds/seed");
+const data = require("../db/data/test-data/index");
+const db = require("../db/connection");
+
+beforeEach(() => {
+  return seed(data);
+});
+
+afterAll(() => {
+  return db.end();
+});
+
+describe("GET /api", () => {
+  test("200: Responds with an object detailing the documentation for each endpoint", () => {
+    return request(app)
+      .get("/api")
+      .expect(200)
+      .then(({ body: endpoints }) => {
+        expect(endpoints).toEqual();
+      });
+  });
+});
