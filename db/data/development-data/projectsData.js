39671c1fd18c050ccb70d86e4a445565ff673982
robharmer3 <rob.harmer3@gmail.com>
2025-09-06 21:17:59 +0100

    url strings updated

--- db/data/development-data/projectsData.js
+++ db/data/development-data/projectsData.js
@@ -4,24 +4,21 @@ module.exports = [
     category: 1,
     description: "Final solo project",
     link: "INSERT LINK HERE",
-    avatar_url:
-      "/home/rob-harmer/Coding/portfoilo-site/rh-portfolio-be/db/assets/NC-NEWS-1.png",
+    avatar_url: "db/assets/NC-NEWS-1.png",
   },
   {
     title: "2Gather",
     category: 1,
     description: "Final group project",
     link: "INSERT LINK HERE",
-    avatar_url:
-      "/home/rob-harmer/Coding/portfoilo-site/rh-portfolio-be/db/assets/2GATHER-1.png",
+    avatar_url: "db/assets/2GATHER-1.png",
   },
   {
     title: "Portfolio",
     category: 2,
     description: "This portfolio project",
     link: "INSERT LINK HERE",
-    avatar_url:
-      "/home/rob-harmer/Coding/portfoilo-site/rh-portfolio-be/db/assets/PORT-1.png",
+    avatar_url: "db/assets/PORT-1.png",
   },
   // {
   //   title: "Top-Trumps",
@@ -35,7 +32,6 @@ module.exports = [
     category: 3,
     description: "First tech test",
     link: "INSERT LINK HERE",
-    avatar_url:
-      "/home/rob-harmer/Coding/portfoilo-site/rh-portfolio-be/db/assets/VERTICODE-1.png",
+    avatar_url: "db/assets/VERTICODE-1.png",
   },
 ];
